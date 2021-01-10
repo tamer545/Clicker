4c0ef7ccb77c101c6317812e535f9487f26c1ad0
tamer545 <timo-angst@bluewin.ch>
2021-01-10 22:33:47 +0100

    clicker

--- src/clicker/Formatter.java
+++ src/clicker/Formatter.java
@@ -1,22 +1,46 @@
 package clicker;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Formatter {
 
     private static final String[] suffixes = {"", "k", "M", "B", "T", "q", "Q", "s", "S", "O", "N"};
+    //private static final String[] suffixes = {"", "k"}; //for large number testing
+
+    private static final BigDecimal _999 = BigDecimal.valueOf(999);
+    private static final BigDecimal THOUSAND = BigDecimal.valueOf(100);
+    private static final BigDecimal TEN = BigDecimal.TEN;
 
     //input: 10 230 000 -> 10.23M
     public String format(double number) {
         int suffixSize = 0;
+        boolean scientific = false;
 
         while (number > 999) {
+
+            if (suffixSize == suffixes.length - 1) {
+                scientific = true;
+                break;
+            }
+
             suffixSize++;
             number /= 1000;
         }
 
-        if(Math.floor(number) == number){
-            return String.format("%.0f%s", number, suffixes[suffixSize]);
+        if (scientific) {
+            int exp = 3 * suffixSize;
+            while (number >= 10) {
+                exp++;
+                number /= 10;
+            }
+            return String.format("%.2fE%d", number, exp);
         } else {
-            return String.format("%.2f%s", number, suffixes[suffixSize]);
+            if (Math.floor(number) == number) {
+                return String.format("%.0f%s", number, suffixes[suffixSize]);
+            } else {
+                return String.format("%.2f%s", number, suffixes[suffixSize]);
+            }
         }
     }
 }
